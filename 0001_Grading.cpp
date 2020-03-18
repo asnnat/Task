5cfadcd0a37d53e7154697184cd15b3c25592984
asn_nat <omsin.natnicha@gmail.com>
2020-03-18 20:20:18 +0700

    Update 0001_Grading.cpp

--- 0001_Grading.cpp
+++ 0001_Grading.cpp
@@ -2,44 +2,25 @@
 main()
 {
 	int a,b,c;
-	scanf("%d\n%d\n%d",&a,&b,&c);
+	scanf("%d%d%d",&a,&b,&c);
 	int x=a+b+c;
-	if(0<=x<=100 && 0<=a<=30 && 0<=b<=30 && 0<=c<=40)
+	if(0<=x<=100)
 	{
-		if(x<80)
-		{
-			if(x>74)
-			{
-				printf("B+");
-			}
-			else if(x>69)
-			{
-				printf("B");
-			}
-			else if(x>64)
-			{
-				printf("C+");
-			}
-			else if(x>59)
-			{
-				printf("C");
-			}
-			else if(x>54)
-			{
-				printf("D+");
-			}
-			else if(x>49)
-			{
-				printf("D");
-			}
-			else
-			{
-				printf("F");
-			}
-		}
-		else
-		{
+		if(x>=80)
 			printf("A");
-		}
+		else if(x>74)
+			printf("B+");
+		else if(x>69)
+			printf("B");
+		else if(x>64)
+			printf("C+");
+		else if(x>59)
+			printf("C");
+		else if(x>54)
+			printf("D+");
+		else if(x>49)
+			printf("D");
+		else
+			printf("F");
 	}
 }
