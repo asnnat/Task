054123653f84956686e19b8e68b9c9d22be95499
asnnat <omsin.natnicha@gmail.com>
2020-04-10 22:01:47 +0700

    PASSED

--- 1027_Short_message_service.c
+++ 1027_Short_message_service.c
@@ -1,110 +1,71 @@
 #include<stdio.h>
-#include<string.h>
-main()
-{
-    int a[7];
-    int b[7];
-    int i,j,k;
+#include<string.h> 
+main()  
+{  
+    int a[7]; 
+    char c[10000];  
+    int sum = 7; 
+    int i,j;
     for(i=0;i<7;i++)
     {
-        a[i]=0; // point
-        b[i]=-1; // -1 not used tp add | 0 added | 1 can not answer
+        a[i]=0;
     }
-    char c[10000];
-    scanf("%s",&c);
-    int check=0;
-    while(check==0)
-    {
-        int l=strlen(c);
-        int d=0;
-        for(i=0;i<l;i++)
-        {
-            if(c[i]=='!')
-            {
-                check++;
-            }
-        }
-        if(check!=0)
+    while(1)  
+    {  
+        scanf("%s",c);  
+        if(c[0] == '!')
         {
             break;
         }
-        for(i=0;i<l;i++)
+        int l=strlen(c);
+        for(i=0;i<l;i++) 
         {
-            if(b[c[i]-'A']!=1)
+            if(a[c[i]-'A']!=1000000)
             {
-                a[c[i]-'A']++;
-                b[c[i]-'A']=0;
+                a[c[i]-'A']++;  
             }
         }
-        int min=30000;
-        int che=0;
-        for(i=0;i<7;i++)
-        {
-            if(min>a[i]&&b[i]==0)
+        if(sum>1)  
+        {  
+            int min=0;  
+            for(i=1;i<7;i++) 
             {
-                min=a[i];
-            }
-            if(b[i]==0)
-            {
-                che++;
-            }
-        }
-        //printf("che=%d ",che);
-        if(che>1)
-        {
-            int temp=0;
-            int NumMin;
-            for(i=0;i<7;i++)
-            {
-                if(min==a[i]&&b[i]==0)
+                if(a[i]<a[min])
                 {
-                    temp++;
-                    NumMin=i;
+                    min = i;  
                 }
-            }
-            //printf("temp=%d ",temp);
-            if(temp==1)
-            {
-                b[NumMin]=1;
-            }
-            //printf("NumMin=%d\n",NumMin);
-        }
-        /*
-        for(i=0;i<7;i++)
-        {
-            printf("%c %d %d\n",'A'+i,a[i],b[i]);
-        }
-        */
-        scanf("%s",&c);
-    }
-    /*
-    for(i=0;i<7;i++)
-    {
-        printf("%c %d %d\n",'A'+i,a[i],b[i]);
-    }
-    */
-   int sumans=0;
-    for(i=0;i<7;i++)
-    {
-        if(b[i]==0)
-        {
-            sumans++;
-        }
-    }
-    printf("%d\n",sumans);
-    for(i=0;i<sumans;i++)
+            } 
+            int count;
+            for(i=0;i<7;i++)  
+            {  
+                count=0;  
+                if(a[i]==a[min]&&i!=min)  
+                {  
+                    count++;  
+                    break;  
+                }  
+            }  
+            if(count==0)  
+            {  
+                a[min]=1000000;  
+                sum--;  
+            }  
+        }  
+    }  
+    printf("%d",sum);  
+    for(i = 0 ; i < sum ; i++)  
     {
         int max=0;
         int ans;
         for(j=0;j<7;j++)
         {
-            if(b[j]==0&&max<a[j])
+            if(a[j]>max&&a[j]!=1000000)
             {
                 max=a[j];
                 ans=j;
             }
         }
-        printf("%c %d\n",'A'+ans,max);
-        b[ans]=1;
+        printf("\n%c %d",'A'+ans,a[ans]);
+        a[ans]=1000000;
     }
-}
+}  
