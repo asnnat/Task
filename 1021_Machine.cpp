e7f88a855bfed1bbd05330a205107a40cfbe7ffb
asnnat <omsin.natnicha@gmail.com>
2020-04-27 20:19:59 +0700

    reject time out

--- 1021_Machine.cpp
+++ 1021_Machine.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 main()
 {
-    vector<int> v;
     int n;
     cin >> n;
-    for(int i=0;i<n;i++)
+    vector<int> v;
+    for(int i = 0 ; i < n ;i++)
     {
         char c;
         cin >> c;
@@ -21,14 +21,15 @@ main()
         {
             if(v.size()==0)
             {
-                cout << "-1" << endl;
+                cout << -1;
             }
             else
             {
-                make_heap(v.begin(),v.end());
-                cout << v.front() << endl;
-                v.erase(v.begin());
+                sort(v.begin(),v.end());
+                cout << v.back();
+                v.pop_back();
             }
+            cout << endl;
         }
     }
 }
