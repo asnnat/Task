474099a618fd3b797f831dbd9c297e805063a88f
asnnat <omsin.natnicha@gmail.com>
2020-05-20 00:03:21 +0700

    NOT YET!!! data base is very big. Can't use array.

--- 1029_Magnet.cpp
+++ 1029_Magnet.cpp
@@ -1,82 +1,65 @@
 #include<iostream>
+#include<stdio.h>
 using namespace std;
 main()
 {
-    int n;
-    int m;
-    int q;
-    cin >> n >> m >> q;
-    struct magnet
+    int n, m, q;
+    scanf("%d %d %d", &n, &m, &q);
+    char magnet[n+1];
+    int i, j;
+    for(i = 1; i <= n; i++)
     {
-        char h;
-        char t;
-    }a[n+1];
-    int b[n+1]; //down
-    int c[n+1]; //up
-    for(int i=1;i<n+1;i++)
-    {
-        a[i].h='N';
-        a[i].t='S';
-        b[i]=0;
-        c[i]=0;
+        magnet[i] = 'N';
     }
-    /*
-    for(int i=1;i<n+1;i++)
+    for(i = 0; i < m; i++)
     {
-        cout << i << ' ' << a[i].h << ' ' << a[i].t << endl;
-    }
-    */
-    for(int i=0;i<m;i++)
-    {
-        int s;
-        int k;
-        cin >> s >> k;
-        for(int j=s;j<s+k;j++)
+        int start, sum;
+        scanf("%d %d", &start, &sum);
+        for(j = start; j <= start+sum-1; j++)
         {
-            char c=a[j].h;
-            a[j].h=a[j].t;
-            a[j].t=c;
+            if(magnet[j] == 'N')
+                magnet[j] = 'S';
+            else
+                magnet[j] = 'N';    
         }
         /*
-        for(int j=1;j<n+1;j++)
+        for(j = 1; j <= n; j++)
         {
-            cout << j << ' ' << a[j].h << ' ' << a[j].t << endl;
+            printf("%d %c\n", j,magnet[j]);
         }
         */
     }
-    for(int i=1;i<n+1;i++)
+    int above[n+1];
+    int under[n+1];
+    for(i = 1; i <= n; i++)
     {
-        if(a[i].t!=a[i+1].h&&i!=n)
+        above[i] = 0;
+        if(i != 1)
         {
-            b[i]++;
-            if(i!=1)
+            if(magnet[i] == magnet[i-1])
             {
-                b[i]+=b[i-1];
+                above[i] += above[i-1]+1;
             }
         }
-        if(a[n-i].h!=a[n-i-1].t&&i!=1)
+        under[n-i+1] = 0;
+        if(n-i+1 != n)
         {
-            c[n-i]++;
-            if(i!=n)
+            if(magnet[n-i+1] == magnet[n-i+2])
             {
-                c[n-i]+=c[n-i+1];
+                under[n-i+1] += under[n-i+2]+1;
             }
         }
     }
-    for(int i=0;i<q;i++)
+    /*
+    for(i = 1; i <= n; i++)
     {
-        int d;
-        cin >> d;
-        int sum=0;
-        if(d!=1)
-        {
-            sum+=b[d-1];
-        }
-        if(d!=n)
-        {
-            sum+=c[d+1];
-        }
-        sum++;
-        cout << sum << endl;
+        printf("%d %c %d %d\n", i, magnet[i], above[i], under[i]);
+    }
+    */
+    for(i = 0; i <q; i++)
+    {
+        int temp;
+        scanf("%d",&temp);
+        printf("%d\n", above[temp]+under[temp]+1);
     }
 }
