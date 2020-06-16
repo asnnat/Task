5cbc65a9b33d74b6cf8874aaca64eebb94684290
asnnat <omsin.natnicha@gmail.com>
2020-06-17 03:15:50 +0700

    rejected

--- 1032_Sudoku.c
+++ 1032_Sudoku.c
@@ -1,152 +1,164 @@
 #include<stdio.h>
 
-int i, j, k;
-struct Sudoku
-{
-    int MayBe[10];
-    int SumMayBe;
-    int ans;
-}a[10][10];
-int allans = 0;
+int a[10][10];
+int b[10][10];
+int y[10];
+int count = 0;
+int i, j, k, l, m, n;
 
-void CheckX(int y, int x)
+void scana()
 {
-    if(a[y][x].ans != 0)
-    {
-        return ;
-    }
-    a[y][x].SumMayBe = 9;
     for(i = 1; i < 10; i++)
     {
-        if(a[y][i].ans != 0)
-        {
-            a[y][x].MayBe[a[y][i].ans] = 0;
-            a[y][x].SumMayBe--;
-        }
-    }
-    if(a[y][x].SumMayBe == 1)
-    {
-        for(i = 1; i < 10; i++)
-        {
-            if(a[y][x].MayBe[i] == 1)
-            {
-                a[y][x].ans = i;
-                allans++;
-                return ;
-            }
-        }
-    }
-}
-
-void CheckY(int y, int x)
-{
-    if(a[y][x].ans != 0)
-    {
-        return ;
-    }
-    a[y][x].SumMayBe = 9;
-    for(i = 1; i < 10; i++)
-    {
-        if(a[i][x].ans != 0)
-        {
-            a[y][x].MayBe[a[i][x].ans] = 0;
-            a[y][x].SumMayBe--;
-        }
-    }
-    if(a[y][x].SumMayBe == 1)
-    {
-        for(i = 1; i < 10; i++)
-        {
-            if(a[y][x].MayBe[i] == 1)
-            {
-                a[y][x].ans = i;
-                allans++;
-                return ;
-            }
-        }
-    }
-}
-
-void CheckZ(int y, int x)
-{
-    if((y+1)%3 != 0 && (x+1)%3 != 0)
-    {
-        return ;
-    }
-    a[y][x].SumMayBe = 9;
-    for(i = y-1; i <= y+1; i++)
-    {
-        for(j = x-1; j <= x+1; j++)
-        {
-            if(a[i][j].ans != 0)
-            {
-                a[y][x].MayBe[a[i][j].ans] = 0;
-                a[y][x].SumMayBe--;
-            }
-        }
-    }
-    if(a[y][x].SumMayBe == 1)
-    {
-        for(i = 1; i < 10; i++)
-        {
-            if(a[y][x].MayBe[i] == 1)
-            {
-                a[y][x].ans = i;
-                allans++;
-                return ;
-            }
-        }
-    }
-}
-
-main()
-{
-    int n;
-    scanf("%d",&n);
-    for(i = 1; i < 10; i++)
-    {
+        y[i] = 0;
         for(j = 1; j < 10; j++)
         {
-            scanf("%d",&a[i][j].ans);
-            if(a[i][j].ans == 0)
+            scanf(" %d", &a[i][j]);
+            if(a[i][j] == 0)
             {
+                y[i]++;
+                count++;
+            }
+        }
+    }
+}
+
+void answera()
+{
+    for(i = 1; i < 10; i++)
+    {
+        for(j = 1; j < 10 && y[i] > 0; j++)
+        {
+            if(a[i][j] == 0)
+            {
+                int temp[10];
                 for(k = 1; k < 10; k++)
                 {
-                    a[i][j].MayBe[k] = 1;
+                    temp[k] = 0;
+                }
+                for(k = 1; k < 10; k++) // Check horizontal
+                {
+                    if(a[i][k] != 0)
+                    {
+                        temp[a[i][k]]++;
+                    }
+                }
+                for(k = 1; k < 10; k++) // Check vertical
+                {
+                    if(a[k][j] != 0)
+                    {
+                        temp[a[k][j]]++;
+                    }
+                }
+                int p, q;
+                if(i >= 1 && i <= 3)
+                {
+                    p = 1;
+                }
+                else if(i >= 4 && i <= 6)
+                {
+                    p = 4;
+                }
+                else
+                {
+                    p = 7;
+                }
+                if(j >= 1 && j <= 3)
+                {
+                    q = 1;
+                }
+                else if(j >= 4 && j <= 6)
+                {
+                    q = 4;
+                }
+                else
+                {
+                    q = 7;
+                }
+                for(k = 0; k < 3; k++)
+                {
+                    for(l = 0; l < 3; l++)
+                    {
+                        if(a[p+k][q+l] != 0)
+                        {
+                            temp[a[p+k][q+l]]++;
+                        }
+                    }
+                }
+                int check = 0;
+                int ans;
+                for(k = 1; k < 10; k++)
+                {
+                    if(temp[k] == 0)
+                    {
+                        ans = k;
+                    }
+                    else
+                    {
+                        check++;
+                    }
+                }
+                if(check == 8)
+                {
+                    a[i][j] = ans;
+                    count--;
+                    y[i]--;
+                    if(count == 0)
+                    {
+                        return ;
+                    }
                 }
             }
-            else
-            {
-                allans++;
-            }
         }
     }
-    while(allans != 81)
-    {
-        for(i = 1; i < 10; i++)
-        {
-            for(j = 1; j < 10; j++)
-            {
-                CheckX(i, j);
-                CheckY(i, j);
-                CheckZ(i, j);
-            }
-        }
-        for(i = 1; i < 10; i++)
-        {
-            for(j = 1; j < 10; j++)
-            {
-                printf("%d ", a[i][j].ans);
-            }
-            printf("\n");
-        }
-    }
+}
+
+int scanAndCheckb()
+{
+    int sum = 0;
     for(i = 1; i < 10; i++)
     {
         for(j = 1; j < 10; j++)
         {
-            printf("%d ",a[i][j].ans);
+            scanf(" %d", &b[i][j]);
+            if(a[i][j] != b[i][j])
+            {
+                sum++;
+            }
+        }
+    }
+    return sum;
+}
+/*
+void printa()
+{
+    for(i = 1; i < 10; i++)
+    {
+        for(j = 1; j < 10; j++)
+        {
+            printf("%d ", a[i][j]);
         }
         printf("\n");
     }
+}
+*/
+main()
+{
+    int N;
+    scanf("%d", &N);
+    scana();
+    while(count != 0)
+    {
+        answera();
+    }
+    //printa();
+    for(k = 1; k <= N; k++)
+    {
+        int x = scanAndCheckb();
+        if(x == 0)
+        {
+            printf("%d\n", k);
+        }
+    }
+    printf("END");
 }
