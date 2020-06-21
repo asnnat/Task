aa13343a2e79991fe0ccc70774b13b62f83cebe9
asnnat <omsin.natnicha@gmail.com>
2020-06-21 14:41:50 +0700

    NOT YET

--- 1090_Cocktail.cpp
+++ 1090_Cocktail.cpp
@@ -5,8 +5,8 @@ main()
 {
     int n;
     cin >> n;
-    map<int,int> m;
-    for(int i=0;i<n;i++)
+    map<int, int> m;
+    for(int i = 0; i < n; i++)
     {
         int x;
         cin >> x;
@@ -14,20 +14,20 @@ main()
     }
     int a;
     cin >> a;
-    unsigned long long int sum=0;
-    map<int,int>::iterator mitr=m.begin();
-    for(map<int,int>::iterator mitr=m.begin();mitr!=m.end();mitr++)
+    unsigned long long int sum = 0;
+    map<int,int>::iterator mit = m.begin();
+    for(map<int,int>::iterator mitr = m.begin(); mitr != m.end(); mitr++)
     {
-        map<int,int>::iterator mit=m.find(a-mitr->first);
-        if(mit!=m.end())
+        map<int,int>::iterator mit = m.find(a-mitr->first);
+        if(mit != m.end())
         {
-            if(mitr->first==mit->first)
+            if(mitr->first == mit->first)
             {
-                sum+=mitr->second*(mitr->second-1)/2;
+                sum += mitr->second*(mitr->second-1)/2;
             }
             else
             {
-                sum+=mitr->second*mit->second;
+                sum += mitr->second*mit->second;
             }
             mitr->second=0;
             mit->second=0;
