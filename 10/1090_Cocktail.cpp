89c21719c44863fa175c634ebab91636e19666ae
Natnicha Faksang <omsin.natnicha@gmail.com>
2021-06-23 11:59:52 +0700

    incomplete 1090 Cocktail (2 Wrong answers)

--- 10/1090_Cocktail.cpp
+++ 10/1090_Cocktail.cpp
@@ -1,37 +1,42 @@
 #include<iostream>
 #include<map>
 using namespace std;
-main()
-{
+
+main(){
     int n;
     cin >> n;
+
     map<int, int> m;
-    for(int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        m[x]++;
+    for(int i = 0; i < n; i++){
+        int temp;
+        cin >> temp;
+
+        m[temp]++;
     }
+    /*
+    for(map<int,int>::iterator mitr = m.begin(); mitr != m.end(); mitr++){
+        cout << mitr->first << " " << mitr->second << endl;
+    }*/
+
     int a;
     cin >> a;
-    unsigned long long int sum = 0;
-    map<int,int>::iterator mit = m.begin();
-    for(map<int,int>::iterator mitr = m.begin(); mitr != m.end(); mitr++)
-    {
-        map<int,int>::iterator mit = m.find(a-mitr->first);
-        if(mit != m.end())
-        {
-            if(mitr->first == mit->first)
-            {
-                sum += mitr->second*(mitr->second-1)/2;
+
+    long long int sum = 0;
+    for(map<int,int>::iterator mitr = m.begin(); mitr != m.end(); mitr++){
+        map<int,int>::iterator mit = m.find(a - mitr->first);
+        
+        if(mit != m.end()){
+            if(mitr->first == mit->first){
+                sum += mitr->second * (mit->second - 1) / 2;
             }
-            else
-            {
-                sum += mitr->second*mit->second;
+            else{
+                sum += mitr->second * mit->second;
             }
-            mitr->second=0;
-            mit->second=0;
+
+            mitr->second = 0;
+            mit->second = 0;
         }
     }
+
     cout << sum;
 }
