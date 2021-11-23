369ceee88f5451d51b9129bf3f264342976c26ae
Natnicha Faksang <omsin.natnicha@gmail.com>
2021-11-23 15:02:42 +0700

    format code (0004_Character_Checker)

--- 00/0004_Character_Checker.c
+++ 00/0004_Character_Checker.c
@@ -1,34 +1,25 @@
 #include<stdio.h>
-main()
-{
+
+main(){
     char a[10001];
-    int i=0;
-    int m=0;
-    int n=0;
-    scanf("%c",&a[0]);
-    while(a[i]>=65&&a[i]<=90 || a[i]>=97&&a[i]<=122)
-    {
-        if(a[i]>=65&&a[i]<=90)
-        {
+    int i = 0;
+    int m = 0;
+    int n = 0;
+
+    scanf("%c", &a[0]);
+    while(a[i] >= 65 && a[i] <= 90 || a[i] >= 97 && a[i] <= 122){
+        if(a[i] >= 65 && a[i] <= 90)
             m++;
-        }
         else
-        {
             n++;
-        }
         i++;
-        scanf("%c",&a[i]);
+        scanf("%c", &a[i]);
     }
-    if(m==i)
-    {
+
+    if(m == i)
         printf("All Capital Letter");
-    }
-    if(n==i)
-    {
+    else if(n == i)
         printf("All Small Letter");
-    }
-    if(m<i&&m>0)
-    {
+    else
         printf("Mix");
-    }
 }
