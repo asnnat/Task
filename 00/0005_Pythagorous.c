389f116329362144868b532fecb634b052767abf
Natnicha Faksang <omsin.natnicha@gmail.com>
2021-11-23 15:16:03 +0700

    format code (0005_Pythagorous)

--- 00/0005_Pythagorous.c
+++ 00/0005_Pythagorous.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<math.h>
-main()
-{
-    double a;
-    double b;
-    scanf("%lf %lf",&a,&b);
-    double c=sqrt((a*a)+(b*b));
-    printf("%lf",c);
+
+main(){
+    double a,b;
+    scanf("%lf %lf", &a, &b);
+
+    double c = sqrt((a * a) + (b * b));
+
+    printf("%lf", c);
 }
