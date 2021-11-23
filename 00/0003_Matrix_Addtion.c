82442c9a520d5459a5637a909bcf2a2677f50fcb
Natnicha Faksang <omsin.natnicha@gmail.com>
2021-11-23 15:02:23 +0700

    format code (0003_Matrix_Addtion)

--- 00/0003_Matrix_Addtion.c
+++ 00/0003_Matrix_Addtion.c
@@ -1,31 +1,26 @@
 #include<stdio.h>
-main()
-{
-    int m;
-    int n;
+
+main(){
+    int m, n;
     scanf("%d %d", &m, &n);
+
     int a[m][n];
-    int i, j;
-    for(i = 0; i < m; i++)
-    {
-        for(j = 0; j < n; j++)
-        {
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
             scanf("%d ", &a[i][j]);
         }
     }
+
     int b[m][n];
-    for(i = 0; i < m; i++)
-    {
-        for(j = 0; j < n; j++)
-        {
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
             scanf("%d", &b[i][j]);
         }
     }
-    for(i = 0; i < m; i++)
-    {
-        for(j = 0; j < n; j++)
-        {
-            printf("%d ", a[i][j]+b[i][j]);
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", a[i][j] + b[i][j]);
         }
         printf("\n");
     }
